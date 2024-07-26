4df7f5211410f341b8a2986ed4a8a1461a735217
juhyxxngg <jh91717@gmail.com>
2024-07-26 17:01:15 +0900

    Add files via upload

---
+++ 1-1.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+void swap_values(int **pptr1,int **pptr2);
+
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+
+	printf("교환 이전: num1=%d, num2=%d\n", num1, num2);
+	swap_values(&ptr1, &ptr2);
+	printf("교환 이후: num1=%d, num2=%d\n", num1, num2);
+	return 0;
+}
+	
+void swap_values(int** pptr1, int** pptr2) {
+	int pa;
+
+	pa = **pptr1;
+	**pptr1 = **pptr2;
+	**pptr2 = pa;
+}
